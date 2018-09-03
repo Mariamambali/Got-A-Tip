9c2dc8b2270572d4eb6115071edb81f5182b1aca
Mariam Ambali <mariamambali@Mariams-MacBook-Pro.local>
2018-09-03 16:58:35 -0400

    Initial Commit

---
+++ Got a Tip?Tests/Got_a_Tip_Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Got_a_Tip_Tests.swift
+//  Got a Tip?Tests
+//
+//  Created by Mariam Ambali on 9/3/18.
+//  Copyright © 2018 Mariam Ambali. All rights reserved.
+//
+
+import XCTest
+@testable import Got_a_Tip_
+
+class Got_a_Tip_Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
