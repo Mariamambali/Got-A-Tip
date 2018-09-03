b20f35bcdcf63059498519dd3f844b47160fb44b
Mariam Ambali <mariamambali@Mariams-MacBook-Pro.local>
2018-09-03 18:32:53 -0400

    added .gitignore .gitattributes

--- Got a Tip?/ViewController.swift
+++ Got a Tip?/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var BillField: UITextField!
+    @IBOutlet weak var TipLabel: UILabel!
+    @IBOutlet weak var TotalLabel: UILabel!
+    
+    @IBOutlet weak var TipControl: UISegmentedControl!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,22 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func onTap(_ sender: Any) {
+      view.endEditing(true)
+    }
+    
+    @IBAction func CalculateTip(_ sender: Any) {
+       
+        let TipPercentages = [0.10, 0.15, 0.20]
 
+        
+let bill = Double(BillField.text!) ?? 0
+        let tip = bill * TipPercentages[TipControl.selectedSegmentIndex]
+let total = bill + tip
+        
+        TipLabel.text = String (format: "$%.2f", tip)
+        TotalLabel.text = String (format: "$%.2f", total)
+    
+    }
 }
 
